6b0d6db14365a8c03cbfab664c7f9b87794021d3
EdVelez.G <EdVelez.G@3271b04e-0b8f-11df-b76f-33ad6b24dacb>
2010-03-25 17:34:20 +0000

    testing some funcs

--- proj/src0/apps/simpleCUDPP/simpleCUDPP.cu
+++ proj/src0/apps/simpleCUDPP/simpleCUDPP.cu
@@ -12,6 +12,12 @@
 #include "cutil.h"
 #include "cudpp.h"
 
+/**
+ * Number of threads per block
+ */
+const int blocksize = 32;
+time_t seconds;
+
 __global__
 void mult_matrix_by_vector( float* a, float *b, float *c, int N )
 {
@@ -44,6 +50,95 @@ void runTest( int argc, char** argv);
 int
 main( int argc, char** argv) 
 {
+
+    /**
+     * Command line arguments must be 1 which is the number of rows 
+     * and columns for a matrix and the size of the vector. 
+     */
+    if ( argc != 2 )
+    {
+        cout<<"usage: "<< argv[0] <<" <size n>\n";
+        return EXIT_FAILURE;
+    }
+
+    int N = atoi(argv[1]);
+    float *a = new float[N*N];
+    float *b = new float[N];
+    float *c = new float[N];
+
+    seconds = time (NULL);
+    srand(seconds);
+
+    for ( int i = 0; i < N*N; ++i )
+    {
+        // calculate a random number between 0 and 1000
+//      a[i] = (float) (rand()%RAND_MAX);
+        a[i] = (float) i;
+    }
+
+    for ( int i = 0; i < N; ++i )
+    {
+        // calculate a random number between 0 and 1000
+//      b[i] = (float) (rand()%RAND_MAX);
+        b[i] = (float) i;
+        c[i] = (float) 0;
+    }
+
+    float *ad, *bd, *cd;
+    const int sizeVec = N*sizeof(float);
+    const int sizeMat = N*sizeVec;
+
+    cudaMalloc( (void**)&ad, sizeMat );
+    cudaMalloc( (void**)&bd, sizeVec );
+    cudaMalloc( (void**)&cd, sizeVec );
+    cudaMemcpy( ad, a, sizeMat, cudaMemcpyHostToDevice );
+    cudaMemcpy( bd, b, sizeVec, cudaMemcpyHostToDevice );
+
+    dim3 dimBlock(blocksize);
+    dim3 dimGrid(ceil(N/(float)blocksize));
+
+    /**
+     * CPU-only version of the program.
+     */
+    //  int idx;
+    //  for ( int i = 0; i < N; ++i )
+    //  {
+    //      for ( int j = 0; j < N; ++j )
+    //      {
+    //          idx = j + i*N;
+    //          c[i] += a[idx]*b[j];
+    //      }
+    //  }
+    //
+    //  cout << "c[:" << 0 << "]=" << c[0] << endl;
+    //  cout << "c[:" << 1 << "]=" << c[1] << endl;
+    //  cout << "c[:" << 2 << "]=" << c[2] << endl;
+    //  cout << "c[:" << 3 << "]=" << c[3] << endl;
+    //  cout << "c[:" << 4 << "]=" << c[4] << endl;
+    //
+    //  cout << endl;
+
+    mult_matrix_by_vector<<<dimGrid, dimBlock>>>( ad, bd, cd, N );
+
+    cudaMemcpy( c, cd, sizeVec, cudaMemcpyDeviceToHost );
+
+    /**
+     * GPU Output.
+     */
+    cout << "c[" << 0 << "]=" << c[0] << endl;
+    cout << "c[" << 1 << "]=" << c[1] << endl;
+    cout << "c[" << 2 << "]=" << c[2] << endl;
+    cout << "c[" << 3 << "]=" << c[3] << endl;
+//  cout << "c[" << 4 << "]=" << c[4] << endl;
+
+    cudaFree( ad ); 
+    cudaFree( bd ); 
+    cudaFree( cd );
+
+    delete[] a;
+    delete[] b;
+    delete[] c;
+
     runTest( argc, argv);
     CUT_EXIT(argc, argv);
 }
