2334fac47612cc3dd799079e1993350d400d49be
EdVelez.G <EdVelez.G@3271b04e-0b8f-11df-b76f-33ad6b24dacb>
2010-03-25 16:09:11 +0000

    bullshit kernel.. just to get started

--- trunk/proj/src0/apps/simpleCUDPP/simpleCUDPP.cu
+++ trunk/proj/src0/apps/simpleCUDPP/simpleCUDPP.cu
@@ -12,15 +12,31 @@
 #include "cutil.h"
 #include "cudpp.h"
 
+__global__
+void mult_matrix_by_vector( float* a, float *b, float *c, int N )
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    float Cvalue = 0;
+    int index;
+
+    /*
+    * Each thread will perform the dot product between the row of the matrix 
+    * and the vector that is being multiplied. 
+    */
+    if ( i < N )
+    {
+        for ( int e = 0; e < N; ++e )
+        {
+            index = e + i*N;
+            Cvalue += a[index]*b[e];
+        }
+        c[i] = Cvalue;
+    }
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 // declaration, forward
 void runTest( int argc, char** argv);
-// 
-// extern "C" 
-// void computeSumScanGold( float *reference, const float *idata, 
-//                         const unsigned int len,
-//                         const CUDPPConfiguration &config);
 
 ////////////////////////////////////////////////////////////////////////////////
 // Program main
@@ -44,34 +60,34 @@ runTest( int argc, char** argv)
     unsigned int memSize = sizeof( float) * numElements; // size of the memory
 
     // allocate host memory
-    float* h_idata = (float*) malloc( memSize); // allocating input data
-    char* h_sdata = (char*) malloc( memSize); // allocating input data
+    float* h_frequencies = (float*) malloc( memSize); // allocating input data
+    char* h_symbols = (char*) malloc( memSize); // allocating input data
 
     // initalizing the memory with the elements
     for (unsigned int i = 0; i < numElements; ++i) 
     {
-		h_idata[i] = (float) (i+1);
-		printf("i = %f\n", h_idata[i]);
+		h_frequencies[i] = (float) (i+1);
+		printf("i = %f\n", h_frequencies[i]);
     }
 	
 	// allocating symbolic data
     for (unsigned int i = 0; i < numElements; ++i) 
     {
-		h_sdata[i] = 'A' + (char)i; // (rand() & 0xf);
-		printf("i = %c\n", h_sdata[i]);
+		h_symbols[i] = 'A' + (char)i; // (rand() & 0xf);
+		printf("i = %c\n", h_symbols[i]);
     }
 
 
-    // allocate device memory
-    float* d_idata;
-    CUDA_SAFE_CALL( cudaMalloc( (void**) &d_idata, memSize));
+    // allocate device memory for frequencies
+    float* d_frequencies; // frequencies
+    CUDA_SAFE_CALL( cudaMalloc( (void**) &d_frequencies, memSize));
     // copy host memory to device
-    CUDA_SAFE_CALL( cudaMemcpy( d_idata, h_idata, memSize,
+    CUDA_SAFE_CALL( cudaMemcpy( d_frequencies, h_frequencies, memSize,
                                 cudaMemcpyHostToDevice) );
 
-    // allocate device memory for result
-    float* d_odata;
-    CUDA_SAFE_CALL( cudaMalloc( (void**) &d_odata, memSize));
+    // allocate device memory for exclusive scan output
+    float* d_exclusiveScan; // exclusive scan output
+    CUDA_SAFE_CALL( cudaMalloc( (void**) &d_exclusiveScan, memSize));
 
     // Initialize the CUDPP Library
     CUDPPHandle theCudpp;
@@ -93,12 +109,12 @@ runTest( int argc, char** argv)
     }
 
     // Run the scan
-    cudppScan(scanplan, d_odata, d_idata, numElements);
+    cudppScan(scanplan, d_exclusiveScan, d_frequencies, numElements);
 
     // allocate mem for the result on host side
     float* h_odata = (float*) malloc( memSize);
     // copy result from device to host
-    CUDA_SAFE_CALL( cudaMemcpy( h_odata, d_odata, memSize,
+    CUDA_SAFE_CALL( cudaMemcpy( h_odata, d_exclusiveScan, memSize,
                                 cudaMemcpyDeviceToHost) );
 
 	for(size_t i = 0; i < numElements; ++i)
@@ -116,9 +132,9 @@ runTest( int argc, char** argv)
     // shut down the CUDPP library
     cudppDestroy(theCudpp);
     
-    free( h_idata);
+    free( h_frequencies);
     free( h_odata);
     // free( reference);
-    CUDA_SAFE_CALL(cudaFree(d_idata));
-    CUDA_SAFE_CALL(cudaFree(d_odata));
+    CUDA_SAFE_CALL(cudaFree(d_frequencies));
+    CUDA_SAFE_CALL(cudaFree(d_exclusiveScan));
 }
